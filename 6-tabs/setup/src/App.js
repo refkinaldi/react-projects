cdf6fe39b141e584de8cb7f5c8e4da455ed99106
refkinaldi <refkinaldi26@gmail.com>
2021-02-01 18:18:33 +0700

    inisiasi folder

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -3,7 +3,26 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
+
+
+
 function App() {
+  const [loading, setLoading] =useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+  
+  const fetchJobs = async () => {
+    const response =await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs(); 
+  }, []);
+
+  
   return <h2>tabs project setup</h2>
 }
 
