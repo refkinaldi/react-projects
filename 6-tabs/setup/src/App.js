7f4cce80849b6f993ca6bf19e40f69870de958b5
refkinaldi <refkinaldi26@gmail.com>
2021-02-02 22:06:27 +0700

    belajar tabs

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -22,8 +22,47 @@ function App() {
     fetchJobs(); 
   }, []);
 
-  
-  return <h2>tabs project setup</h2>
+  if (loading){
+    return(
+      <section className='section loading'>
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+  const{company, dates, duties, title} = jobs[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return <button key={item.id} onClick={()=>setValue(index)} 
+            className={`job-btn ${index === value && 'active-btn'}`}>{item.company}
+            </button>
+          })}
+        </div>
+        {/*job info*/}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return(
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'>
+                </FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
