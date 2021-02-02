b1514c89b0ac9185c4d1132d449a000c84137717
refkinaldi <refkinaldi26@gmail.com>
2021-02-02 22:07:39 +0700

    kepencet enter

--- 6-tabs/setup/src/App.js
+++ 6-tabs/setup/src/App.js
@@ -41,7 +41,8 @@ function App() {
         <div className='btn-container'>
           {jobs.map((item, index) => {
             return <button key={item.id} onClick={()=>setValue(index)} 
-            className={`job-btn ${index === value && 'active-btn'}`}>{item.company}
+            className={`job-btn ${index === value && 'active-btn'}`}>
+              {item.company}
             </button>
           })}
         </div>
